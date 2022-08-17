d43f0721c0cf7a8d617ab93fdef69de18563e007
joao966 <joaooliveira966@gmail.com>
2022-08-17 01:24:25 -0300

    ADD: MVP

---
+++ src/styles/global.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
+
+  @media (max-width: 600px) {
+    .grid {
+      width: 100%;
+      flex-direction: column;
+    }
+  }
+
+  @media (prefers-color-scheme: dark) {
+    .card,
+    .footer {
+      border-color: #222;
+    }
+    .logo img {
+      filter: invert(1);
+    }
+  }
+
+`;
+
+export default GlobalStyle;
---
+++ src/utils/index.ts
@@ -0,0 +1,18 @@
+export const walls = [
+  {
+    title: "Parede 1",
+    subtitle: "Indique as medidas."
+  },
+  {
+    title: "Parede 2",
+    subtitle: "Indique as medidas."
+  },
+  {
+    title: "Parede 3",
+    subtitle: "Indique as medidas."
+  },
+  {
+    title: "Parede 4",
+    subtitle: "Indique as medidas."
+  }
+]; 
